0e42beed6c0f0ddcac1d23d8dae05026a9f169b8
Andrew Kalian <andrewkalian2@gmail.com>
2025-08-31 13:22:41 -0700

    setup

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,7 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./style.css"
+import { StrictMode } from "react";
 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <App />
+    <h1 className="bg-amber-300 w-40 h-20">
+      Something aaaaaa
+    </h1>
+  
   </StrictMode>,
 )
